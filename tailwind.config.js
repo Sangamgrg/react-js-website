13255566a8804e4d89751949c5afb44f6b8e2dad
sangam <sangamgrg89@gmail.com>
2023-10-23 23:45:57 +1100

    First navbar created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*{.js,.ts,.jsx,.tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutralSilver: "#F5F7FA",
+        neutralDGrey: "#4D4D4D",
+        brandPrimary: "#4CAF4F",
+        neutralGrey: "#717171",
+        gray900: "#18191F",
+      },
+    },
   },
   plugins: [],
 }
