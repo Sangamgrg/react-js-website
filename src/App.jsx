300f6aa639f3374d31bd328eea86ba4b76f4f07b
sangam <sangamgrg89@gmail.com>
2023-10-25 23:42:47 +1100

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import NavBar from "./components/NavBar"
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <h1 className='text-3xl font-bold text-brandPrimary'>Hello world!</h1>
       <NavBar />
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
